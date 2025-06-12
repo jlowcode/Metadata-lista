ed473f847aa9e2357e81ffc5d2729ac3d56e0999
David-Lucas17 <lucasehermilda@gmail.com>
2025-06-12 13:48:57 -0300

    config: initial commit

---
+++ metadata.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * MetaDatalist Element
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Fabrik.list.metadata
+ * @copyright   Copyright (C) 2025 Jlowcode Org - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
+
+/**
+ * 	Plugin that displays relevant information from a list when its URL is shared
+ * 
+ * @package     	Joomla.Plugin
+ * @subpackage  	Fabrik.list.metadata
+ * @since       	4.0
+ */
+class PlgFabrik_ListMetadata extends PlgFabrik_List {
+
+    public function __construct(&$subject, $config = array()) 
+    {
+        parent::__construct($subject, $config);
+    }
+
+    /**
+     * Check user can view the read only element OR view in list view
+     *
+     * @param   	String 		$view 		View list/form
+     *
+     * @return  	Bool
+     */
+    public function canView($view = 'list') 
+    {
+        return true;
+    }
+
+    /**
+     * 
+     * 
+     * Increments the access (views) counter
+     * each time the record is loaded.
+     * 
+     * @return 		Void
+    */
+    public function onLoadData(&$args) 
+    {
+        $model = $this->getModel();
+        $table = $model->getTable();
+
+        $title = $table->get('label');
+        $description = $table->get('introduction');
+
+        $this->setOgTags($title, $description);
+        $this->setTwitterTags($title, $description);
+        $this->app->getDocument()->setMetaData('description', strip_tags($description));
+
+        $image = $this->getImagePath();
+        if ($image) {
+            $this->app->getDocument()->setMetaData('og:image', $image);
+            $this->app->getDocument()->setMetaData('twitter:image', $image);
+        }
+
+        return [
+            'title' => $title,
+            'description' => $description
+        ];
+    }
+    
+    /**
+     * Sets the Open Graph meta tags for page title, description, and type.
+     * 
+     * @param   string  $title        The title to set in og:title
+     * @param   string  $description  The description to set in og:description
+     * 
+     * @return  void
+     */
+    public function setOgTags($title, $description)
+    {
+        $this->app->getDocument()->setMetaData('og:title', strip_tags($title));
+        $this->app->getDocument()->setMetaData('og:description', strip_tags($description));
+        $this->app->getDocument()->setMetaData('og:type', 'website');
+    }
+
+    /**
+     * Sets the Twitter meta tags for card type, title, and description.
+     * 
+     * @param   string  $title        The title to set in twitter:title
+     * @param   string  $description  The description to set in twitter:description
+     * 
+     * @return  void
+     */
+    public function setTwitterTags($title, $description)
+    {
+        $this->app->getDocument()->setMetaData('twitter:card', 'summary');
+        $this->app->getDocument()->setMetaData('twitter:title', strip_tags($title));
+        $this->app->getDocument()->setMetaData('twitter:description', strip_tags($description));
+    }
+
+    /**
+     * Retrieves the URL path of the image thumbnail if it exists on the server.
+     * 
+     * @return  string|null  The full URL to the image thumbnail or null if not found.
+     */
+    public function getImagePath()
+    {
+        $model = $this->getModel();
+        $table = $model->getTable();
+        $listId = $table->get('id');
+
+        if (!$listId) {
+            return null;
+        }
+
+        $db = \JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select($db->qn('miniatura'))->from($db->qn('adm_cloner_listas'))->where($db->qn('id_lista') . ' = ' . $db->q($listId));
+        $db->setQuery($query);
+        $miniatura = $db->loadResult();
+
+        if ($miniatura) {
+        $physicalPath = JPATH_SITE . '/' . ltrim($miniatura, '/');
+
+            if (file_exists($physicalPath)) {
+                return \JUri::root() . ltrim($miniatura, '/');
+            }
+        }
+
+        return null;
+    }
+
+}
